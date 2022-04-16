4a9a29558d8617d8198523b8bb8b9b1e42b3d02e
andy <luthandyka.business@gmail.com>
2022-04-17 07:11:08 +0800

    freak

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+vim.cmd [[
+    source ~/.config/nvim/vim/options.vim
+    source ~/.config/nvim/vim/mappings.vim
+]]
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
+require "user.telescope"
